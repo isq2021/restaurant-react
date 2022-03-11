81f1c12547078c281a14a2f9a20357a200ea9942
Isku Esenov <snvislm2021@gmail.com>
2022-03-11 16:48:06 +0600

    fetch workers

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 
 - use github (min 2 commits)
 - add extra functionalities
+- music
+- voice search
+
+** teams **
+
+Ziadin
+Venera
+Abay
+Farabi
+Islam
--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from "react";
-import menu from "./assets/data";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
-  let [selectedCategory, setSelectedCategory] = useState("all");
-  let [foodList, setFoodList] = useState(menu);
+  let [employees, setEmployees] = useState([]);
+  let URL = "https://retoolapi.dev/RuqKld/employees";
+
+  const getData = async () => {
+    try {
+      let response = await axios.get(URL);
+      setEmployees(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    let list = menu.filter((food) => selectedCategory === food.category);
-    setFoodList(list);
-  }, [selectedCategory]);
-  let catList = ["all", ...new Set(menu.map((item) => item.category))];
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let data = employees.map((a) => {
+    return (
+      <div className="card" key={a.name}>
+        <h2>Name : {a.name}</h2>
+        <h3>Salary:{a.salary}</h3>
+        age: {a.level}
+      </div>
+    );
+  });
 
   return (
     <>
-      <div>
-        <h1>Our menu</h1>
-        {catList.map((cat) => (
-          <button onClick={() => setSelectedCategory(cat)}>{cat}</button>
-        ))}
-        {
-          // eslint-disable-next-line array-callback-return
-          foodList.map((food) => {
-            <p>{food.title}</p>;
-          })
-        }
-      </div>
+      <div>{data}</div>
     </>
   );
 }
