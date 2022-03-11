291b1fa98b7e8b0f6d21bae83a713322be25ee64
Isku Esenov <snvislm2021@gmail.com>
2022-03-11 22:29:41 +0600

    nothing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 function App() {
   let [employees, setEmployees] = useState([]);
-  let URL = "https://retoolapi.dev/RuqKld/employees";
+  let [pics, setPics] = useState([]);
+  let URL = "https://retoolapi.dev/c01dit/employees";
+  let PicURL = "https://reqres.in/api/users";
 
   const getData = async () => {
     try {
@@ -15,18 +17,30 @@ function App() {
       console.log(err);
     }
   };
+  const getDataPic = async () => {
+    try {
+      let response = await axios.get(PicURL);
+      setPics(response.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     getData();
+    getDataPic();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   let data = employees.map((a) => {
+    let randomNum = Math.floor(Math.random() * 5);
+    console.log(randomNum);
     return (
-      <div className="card" key={a.name}>
+      <div className="card" key={a.id}>
         <h2>Name : {a.name}</h2>
         <h3>Salary:{a.salary}</h3>
         age: {a.level}
+        <img src={pics[randomNum].avatar} alt="pic" />;
       </div>
     );
   });
